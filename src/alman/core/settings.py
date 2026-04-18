7b05261e38cb274ca5f8d558966275cf5554e13c
Vaywind <09pattt@gmail.com>
2026-04-19 00:06:30 +0700

    feat: 2 files settings system finished

--- src/alman/core/settings.py
+++ src/alman/core/settings.py
@@ -2,8 +2,11 @@ import json
 import shutil
 import copy
 from pathlib import Path
+from typing import Any
 from alman.infrastructure.app_file import root
 from alman.utils.data_utils import sync_dictionaries
+from alman.core.base import *
+
 
 class Settings:
     def __init__(self,
@@ -11,10 +14,9 @@ class Settings:
                  relative_settings_path: Path = Path('settings.json'),
                  app_root: Path = root) -> None:
 
-        self.settings = None
-        self.local = app_root / relative_settings_path
-        self.template = app_root / relative_settings_example_path
-
+        self.local: Path = app_root / relative_settings_path
+        self.template: Path = app_root / relative_settings_example_path
+        self.settings: dict = {}
         self.load_settings()
 
     def restore_settings(self) -> None:
@@ -22,9 +24,11 @@ class Settings:
             shutil.copy2(self.template, self.local)
         else:
             raise FileNotFoundError(f"Program unable to find {str(self.template)}")
+        log.info(f"Settings restored from {str(self.local)}.")
 
-    def load_settings(self) -> None:
+    def load_settings(self) -> Settings:
         if not self.local.exists():
+            log.info(f"Settings file not found, creating new one from template.")
             self.restore_settings()
 
         with open(str(self.local), 'r', encoding='utf-8') as f:
@@ -38,5 +42,127 @@ class Settings:
         self.settings = sync_dictionaries(template=template, target=local, add_new_key=True, remove_current_key=True)
 
         if self.settings != original_local:
+            self.save_settings()
+
+        return self
+
+    def is_valid(self) -> bool:
+        result: bool = True
+        if not LoggingSettings(self).is_valid():
+            result = False
+
+        if not result:
+            raise AttributeError("Settings is not valid")
+        log.debug(f"Current master settings data is valid.")
+        return result
+
+    def save_settings(self) -> None:
+        if self.is_valid():
             with open(self.local, 'w', encoding='utf-8') as f:
                 json.dump(self.settings, f, ensure_ascii=False, indent=4)
+            log.info(f"Settings saved to {str(self.local)} with current settings data.")
+
+
+def merge_settings(master: Settings, branch: SettingsBranch) -> dict:
+    settings_copy: dict = copy.deepcopy(master.settings)
+    branch_dict: dict = branch.get_dict()
+
+    branch_layers: list[dict] = []
+    for i in range(0, len(branch.dict_index)):
+        dict_to_get: list = []
+        for j in range(0, (i+1)):
+            dict_to_get.append(branch.dict_index[j])
+        layer = settings_copy
+        for key in dict_to_get:
+            layer = layer.get(key, {})
+        branch_layers.append(layer)
+
+    new_settings = settings_copy
+    new_branch: dict = sync_dictionaries(template=branch_dict,
+                                         target=branch_layers[-1],
+                                         add_new_key=False,
+                                         remove_current_key=False,
+                                         override_value=True)
+    if len(branch.dict_index) > 1:
+        for i in range(1, len(branch.dict_index)):
+            new_branch[branch.dict_index[-i]] = new_branch
+    new_settings[branch.dict_index[0]] = new_branch
+
+    return new_settings
+
+
+
+class SettingsBranch:
+    def __init__(self, dict_index: list[str], settings: Settings):
+        self.dict_index = dict_index
+        self.dict: dict = {}
+        self.settings = settings
+
+    def load_branch_settings(self) -> SettingsBranch:
+        self.dict = self.settings.settings
+        for key in self.dict_index:
+            self.dict = self.dict.get(key, {})
+
+        for key, value in self.dict.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+            elif type(value) == dict:
+                pass
+            else:
+                pass
+        return self
+
+    def get_dict(self) -> dict:
+        branch_settings = {}
+        for key in self.dict.keys():
+            if hasattr(self, key):
+                branch_settings[key] = getattr(self, key)
+        return branch_settings
+
+    def is_valid(self) -> bool:
+        return True
+
+
+class LoggingSettings(SettingsBranch):
+    def __init__(self, settings: Settings):
+        super().__init__(dict_index=['logging'], settings=settings)
+
+        self.log_level = 'INFO'
+        self.show_path = True
+        self.log_to_file = True
+
+        self.load_branch_settings()
+
+    def is_valid(self) -> bool:
+        if self.log_level in ['DEBUG', 'INFO', 'WARNING', 'ERROR']:
+            pass
+        elif type(self.log_level) is int:
+            pass
+        else:
+            return False
+        if not type(self.show_path) is bool:
+            return False
+        if not type(self.log_to_file) is bool:
+            return False
+        log.debug(f"Logging settings branch is valid.")
+        return True
+
+
+class Option:
+    def __init__(self, space: list, default: Any = None) -> None:
+        self.space = space
+        self.default = default
+        if not default in self.space:
+            self.default = self.space[0]
+        self.selected = self.default
+
+    def restore(self):
+        self.selected = self.default
+
+class Switch(Option):
+    def __init__(self, default: bool) -> None:
+        super().__init__([True, False], default=default)
+
+class Selection(Option):
+    def __init__(self, space: list, default: Any = None) -> None:
+        super().__init__(space=space, default=default)
--- tests/core/test_settings.py
+++ tests/core/test_settings.py
@@ -1,7 +1,9 @@
 import pytest
 import json
 from pathlib import Path
-from alman.core.settings import Settings
+from alman.core.settings import Settings, LoggingSettings, merge_settings
+from alman.core.base import *
+
 
 @pytest.mark.parametrize("expected, template_json, local_json, create_template_file, create_local_file", [
     (
@@ -27,9 +29,9 @@ from alman.core.settings import Settings
     ),
 ])
 def test_settings_loader_creates_local_from_template(tmp_path, expected, template_json, local_json, create_template_file, create_local_file):
-    app_root = Path(tmp_path)
-    template = app_root / "settings.json.example"
-    local = app_root / "settings.json"
+    root = Path(tmp_path)
+    template = root / "settings.json.example"
+    local = root / "settings.json"
 
     if create_template_file:
         template.touch()
@@ -38,8 +40,86 @@ def test_settings_loader_creates_local_from_template(tmp_path, expected, templat
         local.touch()
         local.write_text(json.dumps(local_json))
 
-    settings = Settings(app_root=app_root)
+    settings = Settings(app_root=root)
     assert settings.settings == expected
     with open(str(local), 'r', encoding="utf-8") as f:
         settings_json = json.load(f)
     assert settings_json == expected
+
+
+def test_get_logging_settings(tmp_path):
+    example_template_settings = {
+        "logging" : {
+          "log_level" : "INFO",
+          "show_path" : True,
+          "log_to_file" : True
+        }
+    }
+
+    example_local_settings = {
+        "logging": {
+            "log_level": "INFO",
+            "show_path": False,
+        }
+    }
+
+    root = Path(tmp_path)
+    template = root / "settings.json.example"
+    template.touch()
+    template.write_text(json.dumps(example_template_settings))
+    local = root / "settings.json"
+    local.touch()
+    local.write_text(json.dumps(example_local_settings))
+
+    master_settings = Settings(app_root=root)
+
+    with open(str(local), 'r', encoding="utf-8") as f:
+        local_settings = json.load(f)
+
+    logging_settings = LoggingSettings(settings=master_settings)
+    expected_log_level = local_settings["logging"]["log_level"]
+    expected_show_path = local_settings["logging"]["show_path"]
+    expected_log_to_file = local_settings["logging"]["log_to_file"]
+
+    assert logging_settings.log_level == expected_log_level
+    assert logging_settings.show_path == expected_show_path
+    assert logging_settings.log_to_file == expected_log_to_file
+
+def test_merge_settings(tmp_path):
+    console.rule(f"TEST-MERGE-SETTINGS")
+    example_template_settings = {
+        "logging": {
+            "log_level": "INFO",
+            "show_path": True,
+            "log_to_file": True
+        }
+    }
+
+    expected_local_settings = {
+        "logging": {
+            "log_level": "DEBUG",
+            "show_path": True,
+            "log_to_file": False
+        }
+    }
+
+    root = Path(tmp_path)
+    template = root / "settings.json.example"
+    template.touch()
+    template.write_text(json.dumps(example_template_settings))
+    local = root / "settings.json"
+
+    master_settings = Settings(app_root=root)
+
+    logging_settings = LoggingSettings(master_settings)
+    logging_settings.log_level = "DEBUG"
+    logging_settings.log_to_file = False
+    master_settings.settings = merge_settings(master=master_settings, branch=logging_settings)
+    master_settings.save_settings()
+
+    with open(str(local), 'r', encoding="utf-8") as f:
+        local_settings = json.load(f)
+
+    console.print(local_settings)
+
+    assert master_settings.settings == expected_local_settings
