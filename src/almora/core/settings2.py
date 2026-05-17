c9bfebd8912ef1260ce04a8f58dad1dd6c933386
Vaywind <09pattt@gmail.com>
2026-05-17 19:03:54 +0700

    feat: added settings2 new settings system and SettingsItem method

---
+++ src/almora/core/settings2.py
@@ -0,0 +1,77 @@
+from typing import Any
+
+
+class SettingsItem:
+    def __init__(self, name: str, space: list | None, default: Any):
+        self._name: str = name
+        self._space: list | None = space
+        self._default: Any = default
+        self._value: Any = self._default
+        self._preparing: tuple[bool, Any] | None = None
+
+        self.validate_attribute()
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def space(self) -> list | None:
+        return self._space
+
+    @property
+    def default(self) -> Any:
+        return self._default
+
+    @property
+    def value(self) -> Any:
+        return self._value
+
+    def __str__(self) -> str:
+        return str(f"{self.name} : {self.value}")
+
+    def validate_attribute(self) -> bool:
+        if self._space:
+            if not self._default in self._space:
+                raise AttributeError(
+                    f"{self._name} > default : {self._default} is not a valid setting. default value must be in ({self._space})")
+            if not self._value in self._space:
+                raise AttributeError(
+                    f"{self._name} : {self._value} is not a valid setting. value must be in ({self._space})")
+        return True
+
+    def validate_value(self, value) -> bool:
+        if self._space:
+            if not value in self._space:
+                raise AttributeError(f"{value} is not a valid value. value must be in ({self._space})")
+        return True
+
+    def prepare(self, value: Any) -> SettingsItem:
+        if self.validate_value(value):
+            self._preparing = (True, value)
+        else:
+            raise AttributeError(f"{value} is not a valid value. value must be in ({self._space})")
+        return self
+
+    def unprepare(self):
+        self._preparing = None
+
+    def apply(self) -> None:
+        if self._preparing:
+            valid, value = self._preparing
+            if valid:
+                self._value = value
+            else:
+                raise AttributeError(f"{value} is not a valid value. value must be in ({self._space})")
+        else:
+            raise AttributeError(f"{self._name} have not been prepared.")
+        self.unprepare()
+
+    def get_dict(self) -> dict:
+        result: dict = {
+            "name": self._name,
+            "space": self._space,
+            "default": self._default,
+            "value": self._value
+        }
+        return result
---
+++ tests/core/test_settings2.py
@@ -0,0 +1,151 @@
+import pytest
+from almora.core.settings2 import SettingsItem
+
+@pytest.mark.parametrize("name, space, default", [
+    (
+        "log_to_file",
+        [True, False],
+        True
+    ),
+    (
+        "open_on_start",
+        [True, False],
+        True
+    ),
+    (
+        "log_max_bytes",
+        [1, 2, 4, 8, 16],
+        4
+    ),
+    (
+        "setting",
+        [1, 2, 3, "A", "B"],
+        "A"
+    ),
+    (
+        "test",
+        None,
+        "hello"
+    ),
+    (
+        "test",
+        None,
+        256
+    )
+])
+def test_setting_item_valid_init(name, space, default):
+    item = SettingsItem(name, space, default)
+    assert item.name == name
+    assert item.space == space
+    assert item.default == default
+    assert item.value == default
+
+
+@pytest.mark.parametrize("space, default", [
+    (
+        [True, False],
+        None
+    ),
+    (
+        [1, 2, 4],
+        100
+    ),
+    (
+        [1, 2, 3, "A", "B"],
+        0
+    )
+])
+def test_setting_item_invalid_init(space, default):
+    with pytest.raises(AttributeError):
+        SettingsItem("test", space, default)
+
+@pytest.mark.parametrize("valid, space, default, value", [
+    (
+        True,
+        [True, False],
+        True,
+        False
+    ),
+    (
+        True,
+        [1, 2, 4, 8],
+        4,
+        8
+    ),
+    (
+        True,
+        [1, 2, 3, "A", "B"],
+        2,
+        "A"
+    ),
+    (
+        False,
+        [1, 2, 4, "A", "B"],
+        2,
+        "C"
+    ),
+    (
+        False,
+        [1, 2, 4, 8],
+        4,
+        16
+    ),
+    (
+        True,
+        None,
+        "user",
+        "u s e r"
+    ),
+    (
+        True,
+        None,
+        "user",
+        256
+    ),
+    (
+        True,
+        None,
+        256,
+        3.14
+    ),
+])
+def test_settings_item_set(valid, space, default, value):
+    item = SettingsItem("test", space, default)
+    if not valid:
+        with pytest.raises(AttributeError):
+            item.prepare(value)
+        return
+    prepared_valid, prepared_value = item.prepare(value)._preparing
+    assert prepared_valid
+    assert prepared_value == value
+    item.apply()
+    assert item.value == value
+
+@pytest.mark.parametrize("name, space, default, value", [
+    (
+        "test",
+        [True, False],
+        True,
+        False
+    ),
+    (
+        "log_max_bytes",
+        [1,2,4,8],
+        4,
+        8
+    ),
+    (
+        "test",
+        None,
+        "user",
+        "Patrick"
+    ),
+])
+def test_settings_item_get_dict(name, space, default, value):
+    item = SettingsItem(name, space, default)
+    item.prepare(value).apply()
+    setting_dict = item.get_dict()
+    assert setting_dict["name"] == name
+    assert setting_dict["space"] == space
+    assert setting_dict["default"] == default
+    assert setting_dict["value"] == value
